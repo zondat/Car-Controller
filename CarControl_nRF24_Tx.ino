1573f0c03c962cfa8a9cdc4044dc91bf0a5b638b
zondat <datnguyen101191@gmail.com>
2023-11-27 23:30:46 +0700

    Update CarControl_nRF24_Tx.ino

--- CarControl_nRF24_Tx.ino
+++ CarControl_nRF24_Tx.ino
@@ -3,17 +3,10 @@
 #include "NRFLite.h"
 
 // Joystick
-#define VX_PIN A2
-#define VY_PIN A1
+#define PIN_VX A2
+#define PIN_VY A1
 #define SW_PIN A0
-
-int readJoystick(int pin) {
-  return analogRead(pin) - 512;
-}
-
-double toDegree(double radian) {
-  return radian/PI*180;
-}
+#define ADC_MAX 1023
 
 // nRF24L01
 const static uint8_t RADIO_ID = 1;             // Our radio's id.
@@ -23,55 +16,54 @@ const static uint8_t PIN_RADIO_CSN = 53;
 
 struct RadioPacket // Any packet up to 32 bytes can be sent.
 {
-    uint8_t signalEsc;
-    uint8_t signalServo;
+    int16_t vx;
+    int16_t vy;
 };
 
 NRFLite _radio;
 RadioPacket _radioData;
 
+int offsetX = 0;
+int offsetY = 0;
+int nbSamples = 100;
+void calib() {
+  for (int i=0; i<nbSamples; i++) {
+    offsetX += analogRead(PIN_VX);
+    offsetY += analogRead(PIN_VY);
+  }
+  offsetX = offsetX / nbSamples;
+  offsetY = offsetY / nbSamples;
+}
+
 void setup()
 {
-    Serial.begin(115200);
-    
-    // By default, 'init' configures the radio to use a 2MBPS bitrate on channel 100 (channels 0-125 are valid).
-    // Both the RX and TX radios must have the same bitrate and channel to communicate with each other.
-    // You can run the 'ChannelScanner' example to help select the best channel for your environment.
-    // You can assign a different bitrate and channel as shown below.
-    //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE2MBPS, 100) // THE DEFAULT
-    //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE1MBPS, 75)
-    //   _radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE250KBPS, 0)
-    
-    while (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))
-    {
-        Serial.println("Cannot communicate with radio");
-        delay (1000); // Wait here forever.
-    }
-    Serial.println("Connected...");
-    
-    // _radioData.FromRadioId = RADIO_ID;
+  Serial.begin(115200);
+  while (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN))
+  {
+      Serial.println("Cannot communicate with radio");
+      delay (1000); // Wait here forever.
+  }
+  Serial.println("Connected...");
 
-    // Joystick
-    pinMode(VX_PIN, INPUT);
-    pinMode(VY_PIN, INPUT);
+  // Joystick
+  pinMode(PIN_VX, INPUT);
+  pinMode(PIN_VY, INPUT);
+
+  // calib
+  calib();
 }
 
 void loop()
 {
-    int32_t vx = readJoystick(VX_PIN);
-    int32_t vy = readJoystick(VY_PIN);
+    int16_t vx = analogRead(PIN_VX) - offsetX - ADC_MAX/2;
+    int16_t vy = analogRead(PIN_VY) - offsetY - ADC_MAX/2;
 
     Serial.println("vx: " + String(vx));
     Serial.println("vy: " + String(vy));
-    Serial.println("sqrt(vx*vx + vy*vy): " + String(sqrt(vx*vx + vy*vy)));
-    uint8_t speed = map(sqrt(vx*vx + vy*vy), 0, 722, 0, 180);
-    uint8_t angle = map(toDegree(atan(vx/vy)), 0, 512, -90, 90);
 
-    _radioData.signalEsc = speed;
-    _radioData.signalServo = angle;
-
-    Serial.println("Speed: " + String(speed));
-    Serial.println("Angle: " + String(angle));   
+    _radioData.vx = vx;
+    _radioData.vy = vy;
+  
     if (_radio.send(DESTINATION_RADIO_ID, &_radioData, sizeof(_radioData))) Serial.println("Message sent");
 
     delay(100);
