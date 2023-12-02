a5771bf46a31faa2eb4a9fb67149dfc533535891
zondat <datnguyen101191@gmail.com>
2023-12-02 10:49:21 +0700

    Update CarControl_nRF24_Tx.ino

--- CarControl_nRF24_Tx.ino
+++ CarControl_nRF24_Tx.ino
@@ -12,7 +12,7 @@
 const static uint8_t RADIO_ID = 1;             // Our radio's id.
 const static uint8_t DESTINATION_RADIO_ID = 0; // Id of the radio we will transmit to.
 const static uint8_t PIN_RADIO_CE = 9;
-const static uint8_t PIN_RADIO_CSN = 53;
+const static uint8_t PIN_RADIO_CSN = 10;
 
 struct RadioPacket // Any packet up to 32 bytes can be sent.
 {
